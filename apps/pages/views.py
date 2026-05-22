09371846318dc47912c42aaf5e7e018dd2db58f9
Pavelwell7 <pavelbugaev303@gmail.com>
2026-05-22 18:57:31 +0300

    fix: запретил отправку отзывов для неавторизованных пользователей

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -13,17 +13,17 @@ def make_order(request):
 
 def reviews_view(request):
     if request.method == "POST":
+        if not request.user.is_authenticated:
+            return redirect("pages:reviews")
+
         user_name = request.POST.get("user_name")
         text = request.POST.get("text")
         rating = request.POST.get("rating")
-
         if user_name and text and rating:
             Review.objects.create(
-                user_name=user_name,
-                text=text,
-                rating=int(rating),
+                user_name=user_name, text=text, rating=int(rating)
             )
-            return redirect('pages:reviews')
+            return redirect("pages:reviews")
 
     reviews = Review.objects.all().order_by("-created_at")
     return render(request, "reviews.html", {"reviews": reviews})
